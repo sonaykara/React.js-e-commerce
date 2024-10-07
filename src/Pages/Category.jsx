c48139a40ea0c62165576070063a3b10016b6a21
Sonay Kara <zonaykara@gmail.com>
2024-10-07 13:28:14 +0300

    Pages created.

---
+++ src/Pages/Category.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import Card from "../Components/Card";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getAllCategoryItem } from "../features/productSlice";
+import { useLocation } from "react-router-dom";
+
+function Category() {
+  console.log("category render edildi")
+  const { categoryItem } = useSelector((state) => state.productReducer);
+  const {pathname} = useLocation()
+  console.log(pathname)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllCategoryItem(pathname));
+  }, [pathname]);
+
+  
+  return (
+    <div className="flex-1 p-4">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {categoryItem && categoryItem.length > 0 ? (
+            categoryItem.map((item, index) => <Card key={index} item={item} />)
+          ) : (
+            <div>Yükleniyor...</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../Components/Card";
+import { getAllProducts } from "../features/productSlice";
+import { useEffect } from "react";
+
+function Home() {
+  console.log("home render edildi")
+  const dispatch = useDispatch();
+  const { product } = useSelector((store) => store.productReducer);
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
+
+  return (
+    <div className="flex-1 p-4">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {product && product.length > 0 ? (
+            product.map((item) => (
+              <Card key={item.id} item={item} />
+            ))
+          ) : (
+            <p className="text-center">No products available</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
